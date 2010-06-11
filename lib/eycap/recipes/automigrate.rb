fe9b78d80cdc02bf340ca605045385475663694a
jsmestad <justin.smestad@gmail.com>
2010-06-11 14:51:27 -0600

    remove block

--- lib/eycap/recipes/automigrate.rb
+++ lib/eycap/recipes/automigrate.rb
@@ -10,11 +10,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     result
   end
 
-  define_recipe :automigrate do
-    after 'deploy:finalize_update' do
-      look_for_db_changes
-      after("deploy:symlink_configs", "deploy:migrate") if db_changed?
-    end
+  after 'deploy:finalize_update' do
+    look_for_db_changes
+    after("deploy:symlink_configs", "deploy:migrate") if db_changed?
   end
   
 end
